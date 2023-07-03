ac6c080c42144329e5ba0dedb4efe05f47f15d1c
agneta1991 <pupienytea@gmail.com>
2023-07-03 12:16:05 +0300

    adding dynamic html in javascript

--- javascript.js
+++ javascript.js
@@ -1,25 +1,43 @@
-let books = []
+let books = [];
+const bookAddition = document.getElementById('addBook');
+/*
+
+function newBook(){
+  let div = document.createElement('div');
+  body.appendChild(div);
+
+  let firstP = document.createElement('p');
+  div.appendChild(firstP);
+
+  let secondP = document.createElement('p');
+  div.appendChild(secondP);
+
+  let removeBtn = document.createElement('button')
+  div.appendChild(removeBtn);
+}*/
+
 
 function addBook() {
-    const titleInput = document.querySelector('.title');
-    const authorInput = document.querySelector('.author');
-    const title = titleInput.value;
-    const author = authorInput.value;
+  const titleInput = document.querySelector('.title');
+  const authorInput = document.querySelector('.author');
+  const title = titleInput.value;
+  const author = authorInput.value;
 
-    if (title === '' || author === '') {
-        alert('Please enter both the title and author.');
-        return;
-    }
+  if (title === '' || author === '') {
+    alert('Please enter both the title and author.');
+    return;
+  }
 
-    const book = {
-        title,
-        author,
-    };
+  const book = {
+    title,
+    author,
+  };
 
-    this.books.push(book);
+  books.push(book);
 
-    titleInput.value = '';
-    authorInput.value = '';
+  titleInput.value = '';
+  authorInput.value = '';
+  bookAddition.addEventListener('click', ()=>{
+    
+  })
 }
-addBook()
-console.log(books)
