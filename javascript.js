0be1d53049621e5c616a1a9ebbc2971b69790870
Mohammed roshdy <xmedox20111@gmail.com>
2023-07-03 11:43:35 +0300

    Added stying and initial javascript

--- javascript.js
+++ javascript.js
@@ -1,1 +1,25 @@
-console.log ('im working :) ')
+let books = []
+
+function addBook() {
+    const titleInput = document.querySelector('.title');
+    const authorInput = document.querySelector('.author');
+    const title = titleInput.value;
+    const author = authorInput.value;
+
+    if (title === '' || author === '') {
+        alert('Please enter both the title and author.');
+        return;
+    }
+
+    const book = {
+        title,
+        author,
+    };
+
+    this.books.push(book);
+
+    titleInput.value = '';
+    authorInput.value = '';
+}
+addBook()
+console.log(books)
